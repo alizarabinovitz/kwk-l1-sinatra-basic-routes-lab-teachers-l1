8ef80938c6f90a080bd77132b9322b0e864a4309
Aliza Rabinovitz <alizajessa24@gmail.com>
2018-06-25 14:10:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Aliza"
+  end
+
+  get '/hometown' do
+    "My hometown in Potomac"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Pageant Material"
+  end
 end
